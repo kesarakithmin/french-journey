05e60b73b1293265f124933afb841a7415ed9bd4
kesarakithmin <kesara.kalansooriya@gmail.com>
2026-07-16 15:32:51 +0200

    Add flexible answer validation

--- src/utils/checkAnswer.ts
+++ src/utils/checkAnswer.ts
@@ -1,26 +1,29 @@
-
-export function normalizeAnswer(value:string){
-
-return value
-.toLowerCase()
-.trim()
-.normalize("NFD")
-.replace(/[\\u0300-\\u036f]/g,"")
-
+export function normalizeAnswer(value: string) {
+  return value
+    .toLowerCase()
+    .trim()
+    .normalize("NFD")
+    .replace(/[\\u0300-\\u036f]/g, "")
 }
 
 
 export function checkAnswer(
-user:string,
-accepted:string[]
-){
+  user: string,
+  accepted: string[],
+  type?: "exact" | "personal" | "translation"
+) {
 
-const cleanUser = normalizeAnswer(user)
+  const cleanUser = normalizeAnswer(user)
 
-return accepted.some(
-(answer)=>
-normalizeAnswer(answer) === cleanUser
-)
+  // Personal answers:
+  // Example: My name, city, country
+  if (type === "personal") {
+    return cleanUser.length > 1
+  }
 
+
+  return accepted.some(
+    (answer) =>
+      normalizeAnswer(answer) === cleanUser
+  )
 }
-
